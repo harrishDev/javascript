756cf6c4b01f1b3fa338ec3675688867cb334709
Harrish <harrishoffical@gmail.com>
2023-06-29 21:40:00 +0530

    initial condition

--- Guess-My-Number/starter/script.js
+++ Guess-My-Number/starter/script.js
@@ -1,8 +1,18 @@
 'use strict';
 
-// console.log(document.querySelector('.message').textContent);
+/*// console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 'Correct Answer';
 
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number ⛔';
+  }
+});
