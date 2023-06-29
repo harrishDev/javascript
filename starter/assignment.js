edbbc670b6b5cfe5b4c8e6ec1d381582cc006ce3
Harrish <harrishoffical@gmail.com>
2023-06-29 20:53:17 +0530

    refactored

--- starter/assignment.js
+++
@@ -1,9 +0,0 @@
-/* ASSIGNMENT 1
-let country = 'Argentina';
-let continent = 'South America';
-let population = '43 millions';
-
-console.log(country);
-console.log(continent);
-console.log(population);
-*/
