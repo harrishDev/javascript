51816739203655e1296b6b666f97ace7f0413ce2
Harrish <harrishoffical@gmail.com>
2023-06-30 20:13:30 +0530

    modal issue resolve ⚙

--- Modal/temp2/script.js
+++ Modal/temp2/script.js
@@ -2,6 +2,12 @@
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-Modal');
-const btnsOpenModal = document.querySelectorAll('.open-Model');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.open-modal');
 console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', function () {
+    console.log('Buttons Clicked');
+    modal.classList.remove('hidden');
+  });
