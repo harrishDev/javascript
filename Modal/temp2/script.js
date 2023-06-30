31d7980b19e51380b4293b3a933798ef9784725f
Harrish <harrishoffical@gmail.com>
2023-06-30 20:37:43 +0530

    restructure

--- Modal/temp2/script.js
+++ Modal/temp2/script.js
@@ -6,21 +6,24 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.open-modal');
 console.log(btnsOpenModal);
 
+// closeModal function
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+// OpenModal function
+const openModal = function () {
+  console.log('Buttons Clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
 // modal pop-up
 for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('Buttons Clicked');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+  btnsOpenModal[i].addEventListener('click', openModal);
 
 //   closebtn
-btnCloseModal.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
-
-overlay.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
+btnCloseModal.addEventListener('click', closeModal);
+// overlay close btn
+overlay.addEventListener('click', closeModal);
