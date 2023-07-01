6f0a2035c98bd334591e7838d3ab1db458c82264
Harrish <harrishoffical@gmail.com>
2023-07-01 18:39:49 +0530

    dice roll

--- Pig-Game/script.js
+++ Pig-Game/script.js
@@ -1,11 +1,32 @@
 'use strict';
 
 // selecting elements
-const score0El = document.querySelector('#score--0');
-const score1El = document.getElementById('score--1');
+const score0El = document.getElementById('score--0');
+const score1El = document.querySelector('#score--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 
 // starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+let currentScore = 0;
+
+// rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  //   console.log(`Dice: ${dice}`);
+
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  if (dice != 1) {
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+  }
+});
