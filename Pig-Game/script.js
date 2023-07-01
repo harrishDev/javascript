d4a2a29b64888afa3e7c4f1b14a10d3a464f7235
Harrish <harrishoffical@gmail.com>
2023-07-01 20:50:54 +0530

    resetting the game 🛠

--- Pig-Game/script.js
+++ Pig-Game/script.js
@@ -13,28 +13,25 @@ const btnHold = document.querySelector('.btn--hold');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
 
+let scores, currentScore, activePlayer, playing;
 // starting conditions
 const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
   score0El.textContent = 0;
   score1El.textContent = 0;
   current0El.textContent = 0;
   current1El.textContent = 0;
-  document
-    .querySelector(`.player--${activePlayer}`)
-    .classList.remove('player--winner');
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
   diceEl.classList.add('hidden');
   player0El.classList.add('player--active');
   player1El.classList.remove('player--active');
 };
-
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
-
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -85,6 +82,4 @@ btnHold.addEventListener('click', function () {
 });
 
 // new game
-btnNew.addEventListener('click', function () {
-  init();
-});
+btnNew.addEventListener('click', init);
