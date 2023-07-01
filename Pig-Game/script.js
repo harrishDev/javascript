14b5c4f0e5c619def2c44e54bd6cec8cdb4b2e31
Harrish <harrishoffical@gmail.com>
2023-07-01 19:06:04 +0530

    switch to next player

--- Pig-Game/script.js
+++ Pig-Game/script.js
@@ -1,6 +1,9 @@
 'use strict';
 
 // selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
 const score0El = document.getElementById('score--0');
 const score1El = document.querySelector('#score--1');
 const diceEl = document.querySelector('.dice');
@@ -14,7 +17,10 @@ const current1El = document.getElementById('current--1');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+const score = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -26,7 +32,13 @@ btnRoll.addEventListener('click', function () {
 
   if (dice != 1) {
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
